056615ef232eff82cb852749e47e3906a210b1c4
Igor Pashev <igor.pashev@nexenta.com>
2011-10-25 10:11:20 +0400

    Fixed top dir

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -30,7 +30,7 @@ ARCHIVE_MIRROR = http://dlc.openindiana.org/oi-build/source-archives
 export DOWNLOAD_SEARCH_PATH +=	$(ARCHIVE_MIRROR)
 
 # The workspace starts at the mercurial root
-export WS_TOP ?=		$(shell hg root)/src/icore
+export WS_TOP ?=		$(shell hg root)
 
 # we want our pkg piplines to fail if there is an error
 # (like if pkgdepend fails in the middle of a pipe), but
